90f363a4f716eec8912f194562d3e6c6c791f810
Haden <thomas.j.haden@googlemail.com>
2025-08-25 16:45:45 +0100

    Initial Scaffolding of concrete implementation

---
+++ CheckoutLib/Models/ItemPriceRule.cs
@@ -0,0 +1,16 @@
+﻿namespace Checkout.Core.Models
+{
+    public class ItemPriceRule
+    {
+        public int UnitPrice { get; set; }
+        public int? PromoQuantity { get; set; }
+        public int? PromoPrice { get; set; }
+
+        public ItemPriceRule(int unitPrice, int? promoAmount = null, int? promoPrice = null)
+        {
+            UnitPrice = unitPrice;
+            PromoQuantity = promoAmount;
+            PromoPrice = promoPrice;
+        }
+    }
+}
---
+++ CheckoutLib/Services/Checkout.cs
@@ -0,0 +1,19 @@
+﻿using Checkout.Core.Interfaces;
+
+namespace Checkout.Core.Services
+{
+    public class Checkout : ICheckout
+    {
+        public Checkout() { }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
